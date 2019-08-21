ff6a8b129ea6940af7cc050a92f8248a84c875d3
bitstone0610 <bitstone0610@outlook.com>
2019-08-21 22:30:27 +0200

    initialize

---
+++ src/components/App.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+
+export class App extends React.Component<{}, IState> {
+  constructor(props: {}){
+    super(props);
+    this.state = {
+      currentTask: "",
+      tasks: [],
+    };
+  }
+
+  public handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    this.setState({
+      currentTask: "",
+      tasks: [
+        ...this.state.tasks,
+        this.state.currentTask,
+      ]
+    });
+  }
+
+  public renderTasks(): JSX.Element[] {
+    return this.state.tasks.map(( task: string, index: number ) => {
+      return (
+        <div key={index}>
+          {task}
+        </div>
+      )
+    })
+  }
+  
+  public render(): JSX.Element {
+    return (
+      <div>
+        <h1>React Typescript Todo List</h1>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="Add a Task"
+            value={this.state.currentTask}
+            onChange={(e) => this.setState({ currentTask: e.target.value })} />
+          <button type="submit">Add Task</button>
+          <div>
+            {this.renderTasks()}
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+
+interface IState {
+  currentTask: string;
+  tasks: Array<string>;
+}
